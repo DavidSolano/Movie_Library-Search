98cef7ff684977ba1356b0ce82a10ce59e681472
School Account <dsolanoreyes@my.wctc.edu>
2021-10-05 04:54:20 -0500

    added the add movie function

--- Movies.cs
+++ Movies.cs
@@ -1,12 +1,17 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace Movie_Library_updated
 {
     public class Movies
     {
+        public List<Movies> MoviesList;
+        
         public int MovieID { get; set; }
         
         public string Title { get; set; }
@@ -21,6 +26,37 @@ namespace Movie_Library_updated
                 {
                     csvReader.Context.RegisterClassMap<MoviesClassMap>();
                     var records = csvReader.GetRecords<Movies>().ToList();
+                }
+            }
+        }
+
+        public void Write()
+        {
+            int last = MoviesList.Count;
+            Console.Write("enter movie title> ");
+            string newTitle = Console.ReadLine();
+            
+            Console.Write("enter movie genre> ");
+            string newGenre = Console.ReadLine();
+
+            var records = new List<Movies>
+            {
+                new Movies {MovieID = last + 1, Title = newTitle, Genre = newGenre}
+            };
+            
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false
+            };
+            
+            using (var stream = File.Open(@"Files\\movies.csv", FileMode.Append))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    using (var csv = new CsvWriter(writer, config))
+                    {
+                        csv.WriteRecords(records);
+                    }
                 }
             }
         }
--- Program.cs
+++ Program.cs
@@ -15,5 +15,14 @@ namespace Movie_Library_updated
             
             //display menu
         }
+
+
+        public static void menu()
+        {
+            Console.WriteLine("enter the number of what you'd like to do");
+            Console.WriteLine("1. look for a movie?");
+            Console.WriteLine("2. add a movie?");
+            
+        }
     }
 }
